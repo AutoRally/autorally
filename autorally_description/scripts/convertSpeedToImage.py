e2f81606ca1bd67927538aabb508bab7be5a43c0
Jason Gibson <jgibson37@gatech.edu>
2018-09-13 23:17:04 -0400

    updated track and script

--- autorally_description/scripts/convertSpeedToImage.py
+++ autorally_description/scripts/convertSpeedToImage.py
@@ -16,6 +16,7 @@ def main():
     flag = 0
     for i in range(0, height):
         for j in range(0, (width)):
+            """
             if img[i, j][0] == 0 and img[i,j][1] == 0 and img[i,j][2] == 0:
                 if flag == 0:
                     flag = 1
@@ -25,13 +26,16 @@ def main():
                 if flag == 3:
                     flag = 0
                 elif img[i,j][2] != 150:
-                    alpha_channel[i,j] = 0
+                     alpha_channel[i,j] = 0
                     flag = 2
-        flag = 0
+            flag = 0
+            """
+            if img[i, j][0] != 0 or img[i,j][1] != 0 or img[i,j][2] != 0:
+                alpha_channel[i,j] = 0
 
     # writes out the image
     img_RGBA = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
-    cv2.imwrite("../urdf/blended_texture_ccrf.png", img_RGBA)
+    cv2.imwrite("../urdf/textures/blended_texture_ccrf.png", img_RGBA)
 
 if __name__ == '__main__':
     main()
