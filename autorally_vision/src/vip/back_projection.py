adjust time calculation and fix style

autorally_vision/src/vip/back_projection.py
@@ -22,18 +22,21 @@ fname = '2020-10-15-11-16-39.bag'
 # %% Open bag and extract pose and images
 
 
-def back_projection(pose, cam_info, t_des):
+def back_projection(pose, cam_info, t_des, use_cam_info):
+    # TODO: add metric to npz
     tt, car_pos, car_quat = pose["tt"], pose["pos"], pose["quat"]
-    K, qC, height, width = cam_info["K"], cam_info["qC"], cam_info["height"], cam_info["width"]
+    if use_cam_info:
+        K, qC, height, width = cam_info["K"], cam_info["qC"], cam_info[
+            "height"], cam_info["width"]
+    else:
+        # Override camera values
+        K = np.array([[762.72493376, 0., 640.5],
+                      [0., 762.72493376, 512.5],
+                      [0., 0., 1.]])
 
-    # Override camera values for now:
-    K = np.array([[762.72493376, 0., 640.5],
-                  [0., 762.72493376, 512.5],
-                  [0., 0., 1.]])
-
-    qC = np.array([[0.127, 0., 0.2159]])
-    height = 1024
-    width = 1280
+        qC = np.array([[0.127, 0., 0.2159]])
+        height = 1024
+        width = 1280
     # Frame Definitions:
     #   I: inertial, world frame
     #   B: body, car frame
@@ -47,6 +50,7 @@ def back_projection(pose, cam_info, t_des):
 
     # Get vehicle pose at future times, t2 > t1
     q_I_t2 = car_pos[:, (tix + 1):]
+    # TODO: labeling goes here
 
     # Get vector from t1 to t2 in body frame
     R_B_I = quat2mat(qinv(t1_quat))  # get quaternion to t1
@@ -61,7 +65,8 @@ def back_projection(pose, cam_info, t_des):
 
     # Find image coordinates from camera frame
     lam_uv = np.dot(K, p_C_t2)  # get raw projection
-    uv = lam_uv / lam_uv[2, :]  # divide by 3rd term to account for distance param
+    uv = lam_uv / lam_uv[2,
+                  :]  # divide by 3rd term to account for distance param
 
     # filter out points outside of FOV
     inside_h = (uv[0, :] < height) & (uv[0, :] > 0)  # inside height
@@ -81,9 +86,12 @@ def back_projection(pose, cam_info, t_des):
 
 def quat2mat(q):
     qx, qy, qz, qw = q[0], q[1], q[2], q[3]
-    return [[qw ** 2 + qx ** 2 - qy ** 2 - qz ** 2, 2 * qx * qy - 2 * qw * qz, 2 * qx * qz + 2 * qw * qy],
-            [2 * qx * qy + 2 * qw * qz, qw ** 2 - qx ** 2 + qy ** 2 - qz ** 2, 2 * qy * qz - 2 * qw * qx],
-            [2 * qx * qz - 2 * qw * qy, 2 * qy * qz + 2 * qw * qx, qw ** 2 - qx ** 2 - qy ** 2 + qz ** 2]]
+    return [[qw ** 2 + qx ** 2 - qy ** 2 - qz ** 2, 2 * qx * qy - 2 * qw * qz,
+             2 * qx * qz + 2 * qw * qy],
+            [2 * qx * qy + 2 * qw * qz, qw ** 2 - qx ** 2 + qy ** 2 - qz ** 2,
+             2 * qy * qz - 2 * qw * qx],
+            [2 * qx * qz - 2 * qw * qy, 2 * qy * qz + 2 * qw * qx,
+             qw ** 2 - qx ** 2 - qy ** 2 + qz ** 2]]
 
 
 def qinv(q):

autorally_vision/src/vip/labeling.py
@@ -13,6 +13,7 @@ import cv2 as cv
 
 npz_exists = False
 
+
 def main(isTrack, dt):
     # get bagfile
 
@@ -39,7 +40,8 @@ def main(isTrack, dt):
     times, metrics, disturbance = roughness.get_labels(bag, dt)
     for i in range(len(times)):
         labels[times[i]] = metrics[i]
-    # Second pass: generate posefile and camfile
+
+    # Second pass: generate posefile and camfile, from rosbag_dump.py
     # TODO: Functionize this setup
 
     if not npz_exists:
@@ -48,39 +50,28 @@ def main(isTrack, dt):
 
         # Print out topics if needed
         print(str(bag.get_type_and_topic_info()[1].keys()))
-        for topic, msg, t in bag.read_messages(topics=['/tf_static']):
+        if isTrack:
+            for topic, msg, t in bag.read_messages(topics=['/tf_static']):
+                qC = np.array([[msg.transforms[1].transform.translation.x,
+                                msg.transforms[1].transform.translation.y,
+                                msg.transforms[1].transform.translation.z]])
 
-            qC = np.array([[msg.transforms[1].transform.translation.x,
-                            msg.transforms[1].transform.translation.y,
-                            msg.transforms[1].transform.translation.z]])
-
-            # %% Camera Intrinsic Matrix
-            numsg = 1
-            for topic, msg, t in bag.read_messages(topics=['/left_camera/camera_info']):
-                numsg -= 1
-                if numsg < 1:
-                    break
-            K = np.reshape(msg.K, (3, 3))
-            height = msg.height
-            width = msg.width
+                K = np.reshape(msg.K, (3, 3))
+                height = msg.height
+                width = msg.width
 
         # %% Pose Information and Images
         state_topics = ["/ground_truth/state"]
         image_topics = ["/left_camera/image_raw"]
 
-        # TODO: Fix assignments of time here to align with everywhere else
-        t0 = bag.get_start_time()
-        tf = bag.get_end_time()
-
         # iterate through bag to get poses
         x = y = z = t1 = t2 = np.array([])
         quat = np.empty((4, 1))
 
-        for topic, msg, t in bag.read_messages(start_time=rospy.Time(t0),
-                                               end_time=rospy.Time(tf),
-                                               topics=state_topics + image_topics):
+        for topic, msg, t in bag.read_messages(
+                topics=state_topics + image_topics):
             if topic in state_topics:
-                t2 = np.append(t2, rospy.Time.to_sec(t))
+                t2 = np.append(t2, msg.header.stamp.to_time())
                 x = np.append(x, msg.pose.pose.position.x)
                 y = np.append(y, msg.pose.pose.position.y)
                 z = np.append(z, msg.pose.pose.position.z)
@@ -91,8 +82,10 @@ def main(isTrack, dt):
                 quat = np.hstack((quat, new_quat))
 
             elif topic in image_topics:
-                img = np.frombuffer(msg.data, dtype='uint8').reshape(msg.height, msg.width, 3)
-                tstr = str(rospy.Time.to_sec(t))
+                img = np.frombuffer(msg.data, dtype='uint8').reshape(msg.height,
+                                                                     msg.width,
+                                                                     3)
+                tstr = str(msg.header.stamp.to_time())
                 outfile = outpath + "images/" + tstr + ".png"
                 #        cv.imwrite(outfile,cv.cvtColor(img, cv.COLOR_RGB2BGR))
                 cv.imwrite(outfile, img)
@@ -101,16 +94,16 @@ def main(isTrack, dt):
         pos = np.vstack((x, y, z))
         # Save output
         np.savez(posefile, pos=pos, quat=quat, tt=t2)
-        np.savez(camfile, K=K, qC=qC, height=height, width=width)
+        # np.savez(camfile, K=K, qC=qC, height=height, width=width)
+        # TODO: reimplement cam_info and camfile properly
         cam_info = {"K": K, "qC": qC, "height": 1024, "width": 1280}
 
     # Third pass: get back prop
     pose = np.load(posefile)
-    cam_info = np.load(camfile)
-
-
-
+    # cam_info = np.load(camfile)
+    for t_des in times:
+        x, y = backProp.back_projection(pose, None, t_des, False)
 
 
 if __name__ == '__main__':
-    main(False, 1)
+    main(True, 1)

autorally_vision/src/vip/rosbag_dump.py
@@ -62,7 +62,7 @@ for topic, msg, t in bag.read_messages(start_time=rospy.Time(t0),
                                        end_time=rospy.Time(tf),
                                        topics=state_topics + image_topics):
     if topic in state_topics:
-        t2 = np.append(t2, rospy.Time.to_sec(t)) #change to header timestamp
+        t2 = np.append(t2, rospy.Time.to_sec(t))
         x = np.append(x, msg.pose.pose.position.x)
         y = np.append(y, msg.pose.pose.position.y)
         z = np.append(z, msg.pose.pose.position.z)
@@ -73,7 +73,8 @@ for topic, msg, t in bag.read_messages(start_time=rospy.Time(t0),
         quat = np.hstack((quat, new_quat))
 
     elif topic in image_topics:
-        img = np.frombuffer(msg.data, dtype='uint8').reshape(msg.height, msg.width, 3)
+        img = np.frombuffer(msg.data, dtype='uint8').reshape(msg.height,
+                                                             msg.width, 3)
         tstr = str(rospy.Time.to_sec(t))
         outfile = outpath + "images/" + tstr + ".png"
         #        cv.imwrite(outfile,cv.cvtColor(img, cv.COLOR_RGB2BGR))

autorally_vision/src/vip/roughness.py
@@ -20,8 +20,8 @@ def get_labels(bagfile, timestep):
     time_relative = np.array([])
     time_ros = np.array([])
     t0 = 0
-    dt = timestep  # timestep analysed, to be tied into back projection time stamp
-    running_squared_data = np.array([])  # measurements used in calculation of metric
+    dt = timestep
+    running_squared_data = np.array([])  # used in calculation of metric
     first_entry = True
     for topic, msg, t in bagfile.read_messages(topics=['/imu/imu']):
         if first_entry:
@@ -30,14 +30,17 @@ def get_labels(bagfile, timestep):
         imu_data = msg
         current_time = msg.header.stamp.to_time() - t0
         vertical_acceleration = imu_data.linear_acceleration.z + 9.81
-        running_squared_data = np.append(running_squared_data, vertical_acceleration * vertical_acceleration)
+        running_squared_data = \
+            np.append(running_squared_data,
+                      vertical_acceleration * vertical_acceleration)
         disturbance_data = np.append(disturbance_data, vertical_acceleration)
         time_relative = np.append(time_relative, current_time)
         time_ros = np.append(time_ros, msg.header.stamp.to_time())
         # if dt time has not passed yet we do not have enough data
         if current_time > dt:
             # calculate metric
-            roughness_metric = (np.average(running_squared_data, returned=True))[0]
+            roughness_metric = \
+                (np.average(running_squared_data, returned=True))[0]
             # print("roughness_metric: " + str(roughness_metric))
             roughness_data = np.append(roughness_data, roughness_metric)
             # take off LRU
@@ -49,12 +52,13 @@ def get_labels(bagfile, timestep):
     #     roughness_data = np.append(roughness_data, 0.0)
     return time_ros, roughness_data, disturbance_data
 
+
 if __name__ == '__main__':
     # bagfile directory
     fpath = '/home/todd/autorally/'
 
     # bagfile
-    fname = fpath + 'alpha_autorally0_2020-07-23-16-27-57_0.bag'  # from track not sim
+    fname = fpath + 'alpha_autorally0_2020-07-23-16-27-57_0.bag'  # trackbag
 
     bag = rosbag.Bag(fname)
 